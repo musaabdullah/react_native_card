120b301cefb4da9d245aa0f172b18e0ca32ba6ad
Musa Abdullah <60151264+musaabdullah@users.noreply.github.com>
2022-01-01 17:09:05 +0300

    update app.txt

--- App.js
+++ App.js
@@ -13,88 +13,11 @@ import ButtonBuy from './ButtonBuy';
 import Cancel from './Cancel';
 import colors from './colors';
 
-const now = new Date();
-const CARDS = [
-  {
-    image: {
-      uri: 'http://www.gamespersecond.com/media/2011/07/battlefield-3-poster.jpg',
-    },
-    title:
-      'Mollit reprehenderit ex aute pariatur proident laboris excepteur id esse. Deserunt nulla nulla laboris ipsum qui ipsum voluptate fugiat nisi eiusmod nisi fugiat voluptate. In ullamco enim laborum tempor do sint amet officia velit duis et. Labore commodo irure commodo aliquip irure cillum excepteur est laboris occaecat dolore et velit. Consequat deserunt aliquip dolor ad. Aliqua et adipisicing sunt voluptate ipsum magna magna fugiat officia tempor dolor velit.',
-    height: 300,
-    renderItem: ({item, index}) => (
-      <View style={{marginVertical: 15}}>
-        <Text
-          style={{
-            paddingHorizontal: 10,
-            fontWeight: 'bold',
-            fontSize: 15,
-            color: 'black',
-          }}>
-          Customizable Content
-        </Text>
-      </View>
-    ),
-  },
-
-  {
-    image: {
-      uri: 'http://www.gamespersecond.com/media/2011/07/battlefield-3-poster.jpg',
-    },
-    title:
-      'Mollit reprehenderit ex aute pariatur proident laboris excepteur id esse. Deserunt nulla nulla laboris ipsum qui ipsum voluptate fugiat nisi eiusmod nisi fugiat voluptate. In ullamco enim laborum tempor do sint amet officia velit duis et. Labore commodo irure commodo aliquip irure cillum excepteur est laboris occaecat dolore et velit. Consequat deserunt aliquip dolor ad. Aliqua et adipisicing sunt voluptate ipsum magna magna fugiat officia tempor dolor velit.',
-    height: 300,
-    renderItem: ({item, index}) => (
-      <View style={{marginVertical: 15}}>
-        <Text
-          style={{
-            paddingHorizontal: 10,
-            fontWeight: 'bold',
-            fontSize: 15,
-            color: 'black',
-          }}>
-          Image date
-        </Text>
-      </View>
-    ),
-  },
-];
 const App = () => {
   return (
-    <CardList
-      listProps={{
-        ListHeaderComponent: () => (
-          <View style={{padding: 16, paddingBottom: 0}}>
-            <Text
-              style={{
-                fontSize: 13,
-                color: 'rgba(0, 0, 0, 0.5)',
-              }}>
-              {now.toDateString()}
-            </Text>
-            <Text style={{fontSize: 32, fontWeight: 'bold'}}>Featured</Text>
-          </View>
-        ),
-      }}
-      data={CARDS}
-      renderItem={({item, index}) => {
-        if (item.renderItem) return item.renderItem({item, index});
-
-        return (
-          <View>
-            <Text>Default Content</Text>
-          </View>
-        );
-      }}
-      renderDetails={({item, index}) => (
-        /* You can also provide custom content per item */
-        <View style={{paddingVertical: 30, paddingHorizontal: 16}}>
-          <Text style={{color: 'rgba(0, 0, 0, 0.7)', fontSize: 18}}>
-            {item.title}
-          </Text>
-        </View>
-      )}
-    />
+    <View>
+      <Text>Hello world</Text>
+    </View>
   );
 };
 
